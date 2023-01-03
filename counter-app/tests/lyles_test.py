5756eb0e8faefd5b52674ccc069f151ef788fc03
mackinnonlyle <97019904+mackinnonlyle@users.noreply.github.com>
2023-01-02 21:13:25 -0400

    new test added2

--- counter-app/tests/lyles_test.py
+++ counter-app/tests/lyles_test.py
@@ -3,33 +3,25 @@ import boto3
 import pytest
 import lambda_handler
 
-
-def test_lambda_handler():
-    # Create a mock event to test the function with
-    event = {}
-
-    # Create a mock context object to test the function with
-    context = {}
-
-    # Call the lambda_handler function with the mock event and context objects
-    lambda_handler(event, context)
-
-    # Connect to the DynamoDB table
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('lyles-resume-challenge')
-
-    # Retrieve the item from the table
-    response = table.get_item(
-        Key = {
-            'ID':'visits'
+def test_parse_event():
+    # Test with valid input
+    valid_event = {
+        "httpMethod": "GET",
+        "headers": {
+            "Accept": "application/json",
+        },
+        "queryStringParameters": {
+            "param1": "value1",
+            "param2": "value2"
         }
-    )
-    
-    # Get the visit count from the item
-    visit_count = response['Item']['counter'] 
+    }
+    assert app.parse_event(valid_event) == ("GET", {"param1": "value1", "param2": "value2"})
 
-    # Check if the visit count has been incremented by 1
-    assert int(visit_count) == 1
-
-    # Print a success message
-    print("Testing has been successful!")
+    # Test with invalid input
+    invalid_event = {
+        "httpMethod": "POST",
+        "headers": {
+            "Accept": "application/xml",
+        }
+    }
+    assert app.parse_event(invalid_event) == ("POST", None)
