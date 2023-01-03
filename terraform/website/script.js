5b23765c11a16a127502f45aa44a4b6fafc4dbd9
mackinnonlyle <97019904+mackinnonlyle@users.noreply.github.com>
2023-01-03 18:05:59 -0400

    11

---
+++ terraform/website/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+fetch('https://dnrpe6jq8b.execute-api.us-east-1.amazonaws.com/Prod/visit', {
+  method: 'POST'
+})
+  .then(response => response.json())
+  .then(visitor_counter => {
+    console.log('**** VISITOR_COUNTER', visitor_counter);
+    const visitsElement = document.querySelector('#visits');
+    if (visitsElement) {
+      visitsElement.textContent = visitor_counter.visit_count;
+    } else {
+      console.error("Element with 'visits' ID not found in the DOM");
+    }
+  })
+  .catch(error => {
+    console.log('Error');
+    console.error(error);
+  });
