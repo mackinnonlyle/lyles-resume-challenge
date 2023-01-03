1e9122b7a17daab1f92bc843073c3d70f8676beb
mackinnonlyle <97019904+mackinnonlyle@users.noreply.github.com>
2023-01-02 20:44:33 -0400

    new test added

---
+++ counter-app/tests/lyles_test.py
@@ -0,0 +1,34 @@
+import boto3
+
+def test_lambda_handler():
+    # Create a mock event to test the function with
+    event = {}
+
+    # Create a mock context object to test the function with
+    context = {}
+
+    # Call the lambda_handler function with the mock event and context objects
+    lambda_handler(event, context)
+
+    # Connect to the DynamoDB table
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('lyles-resume-challenge')
+
+    # Retrieve the item from the table
+    response = table.get_item(
+        Key = {
+            'ID':'visits'
+        }
+    )
+    
+    # Get the visit count from the item
+    visit_count = response['Item']['counter'] 
+
+    # Check if the visit count has been incremented by 1
+    assert int(visit_count) == 1
+
+    # Print a success message
+    print("Test successful!")
+
+# Call the test function
+test_lambda_handler()
